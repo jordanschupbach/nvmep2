bf50d36f05d5baa6f2c136e81d13a086ab497808
jordanschupbach <jordans1882@gmail.com>
2025-09-12 14:08:48 -0600

    add smart splits

--- nix/neovim-overlay.nix
+++ nix/neovim-overlay.nix
@@ -38,7 +38,9 @@ with final.pkgs.lib; let
   # }
   all-plugins = with pkgs.vimPlugins; [
 
+    smart-splits-nvim
     nvim-tree-lua
+
     # plugins from nixpkgs go in here.
     # https://search.nixos.org/packages?channel=unstable&from=0&size=50&sort=relevance&type=packages&query=vimPlugins
     nvim-treesitter.withAllGrammars
