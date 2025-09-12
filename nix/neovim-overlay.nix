568d793ff2004bb26fbd1c359e23dec4fd5e4d9d
jordanschupbach <jordans1882@gmail.com>
2025-09-12 14:22:13 -0600

    Add nvim navic dep back

--- nix/neovim-overlay.nix
+++ nix/neovim-overlay.nix
@@ -41,6 +41,7 @@ with final.pkgs.lib; let
     vim-slime
     smart-splits-nvim
     nvim-tree-lua
+    nvim-navic
 
     # plugins from nixpkgs go in here.
     # https://search.nixos.org/packages?channel=unstable&from=0&size=50&sort=relevance&type=packages&query=vimPlugins
