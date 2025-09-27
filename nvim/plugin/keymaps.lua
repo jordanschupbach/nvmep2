ce285ba120d687f807078e08c3294b1893728295
jordanschupbach <jordans1882@gmail.com>
2025-09-27 07:40:39 -0600

    Modify terminal to normal mode binding

--- nvim/plugin/keymaps.lua
+++ nvim/plugin/keymaps.lua
@@ -116,7 +116,7 @@ keymap.set('n', '<leader>fq', function()
 end, { silent = true, desc = '[f]loating windows: [q]uit/close all' })
 
 -- Remap Esc to switch to normal mode and Ctrl-Esc to pass Esc to terminal
-keymap.set('t', '<Esc>', '<C-\\><C-n>', { desc = 'switch to normal mode' })
+keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'switch to normal mode' })
 keymap.set('t', '<C-Esc>', '<Esc>', { desc = 'send Esc to terminal' })
 
 -- Shortcut for expanding to current buffer's directory in command mode
