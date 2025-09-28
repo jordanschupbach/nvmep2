2a18bf28c42276de85cd88ba3bdb358c39054d8f
jordanschupbach <jordans1882@gmail.com>
2025-09-28 14:48:25 -0600

    Add url open binding

--- nvim/plugin/url-open.lua
+++ nvim/plugin/url-open.lua
@@ -1,7 +1,13 @@
 
+
+local function mymap(mode, key, value)
+  vim.keymap.set(mode, key, value, { silent = true, remap = true })
+end
+
 local status_ok, url_open = pcall(require, "url-open")
 if not status_ok then
   return
 end
 url_open.setup ({})
 
+mymap("n", "<A-o>", "<cmd>URLOpenUnderCursor<CR>")
