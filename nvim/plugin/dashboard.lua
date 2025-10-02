3e831009dabbd9edeadf691af087b6f9c4cf98d0
jordans1882 <jordans1882@gmail.com>
2025-10-02 16:37:08 -0600

    Add dashboard back

--- nvim/plugin/dashboard.lua
+++ nvim/plugin/dashboard.lua
@@ -1,21 +1,34 @@
 local bannertext = {
+  '                                                       NVIM v0.10.2',
   '',
+  '                                       Nvim is open source and freely distributable',
+  '                                                 https://neovim.io/#chat',
+  '',
+  '                                      type  :help nvim<Enter>       if you are new!',
+  '                                      type  :checkhealth<Enter>     to optimize Nvim',
+  '                                      type  :q<Enter>               to exit',
+  '                                      type  :help<Enter>            for help',
+  '',
+  '                                     type  :help news<Enter> to see changes in v0.10',
+  '',
+  '                                              Help poor children in Uganda!',
+  '                                      type  :help iccf<Enter>       for information',
 }
 
--- require('dashboard').setup {
---   theme = 'doom',
---   config = {
---     header = bannertext,
---     center = {
---       {
---         icon = ' ',
---         desc = 'Projects',
---         key = 'p',
---         -- keymap = 'SPC f d',
---         key_format = ' %s',
---         action = 'Telescope project',
---       },
---     },
---     footer = {}, --your footer
---   },
--- }
+require('dashboard').setup {
+  theme = 'doom',
+  config = {
+    header = bannertext,
+    center = {
+      {
+        icon = ' ',
+        desc = 'Projects',
+        key = 'p',
+        -- keymap = 'SPC f d',
+        key_format = ' %s',
+        action = 'Telescope project',
+      },
+    },
+    footer = {}, --your footer
+  },
+}
