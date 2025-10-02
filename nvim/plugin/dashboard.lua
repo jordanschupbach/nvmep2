bd77433fb08c50b5a0e1c8391c19d68b5ea21ce0
jordans1882 <jordans1882@gmail.com>
2025-10-02 16:31:03 -0600

    Remove dashboard config

--- nvim/plugin/dashboard.lua
+++ nvim/plugin/dashboard.lua
@@ -2,20 +2,20 @@ local bannertext = {
   '',
 }
 
-require('dashboard').setup {
-  theme = 'doom',
-  config = {
-    header = bannertext,
-    center = {
-      -- {
-      --   icon = ' ',
-      --   desc = 'Projects',
-      --   key = 'p',
-      --   -- keymap = 'SPC f d',
-      --   key_format = ' %s',
-      --   action = 'Telescope project',
-      -- },
-    },
-    footer = {}, --your footer
-  },
-}
+-- require('dashboard').setup {
+--   theme = 'doom',
+--   config = {
+--     header = bannertext,
+--     center = {
+--       {
+--         icon = ' ',
+--         desc = 'Projects',
+--         key = 'p',
+--         -- keymap = 'SPC f d',
+--         key_format = ' %s',
+--         action = 'Telescope project',
+--       },
+--     },
+--     footer = {}, --your footer
+--   },
+-- }
