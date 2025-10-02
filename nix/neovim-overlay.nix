e7ffdc2e541affa583d3fea42bfce662eab1f093
jordans1882 <jordans1882@gmail.com>
2025-10-02 16:33:13 -0600

    Remove dashboard plugin

--- nix/neovim-overlay.nix
+++ nix/neovim-overlay.nix
@@ -90,7 +90,7 @@ with final.pkgs.lib; let
       # orgmode
       # jupytext-nvim
 
-      dashboard-nvim # https://github.com/nvimdev/dashboard-nvim/
+      # dashboard-nvim # https://github.com/nvimdev/dashboard-nvim/
       text-case-nvim # https://github.com/johmsalas/text-case.nvim
       friendly-snippets # https://github.com/rafamadriz/friendly-snippets
       asyncrun-vim # https://github.com/skywind3000/asyncrun.vim
