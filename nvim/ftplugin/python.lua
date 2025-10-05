b232d6125e738f1982e53d9dcee9f82b2c087630
jordanschupbach <jordans1882@gmail.com>
2025-10-04 21:15:38 -0600

    Fix python lsp

--- nvim/ftplugin/python.lua
+++ nvim/ftplugin/python.lua
@@ -1,5 +1,3 @@
-
-
 -- local function mymap(mode, key, value)
 --   vim.keymap.set(mode, key, value, { silent = true, remap = true })
 -- end
@@ -8,14 +6,17 @@ local root_files = {
   '.git',
 }
 
-
 pythonPath = function()
-  local handle = io.popen("which python")
-  local result = handle:read("*a")
+  local handle = io.popen("nix develop . --command bash -c 'which python' 2>/dev/null")
+  local result = handle:read('*a')
   handle:close()
 
-  if result and result ~= "" then
-    return result:gsub("%s+", "") -- Trim any whitespace
+  -- local handle = io.popen("which python")
+  -- local result = handle:read("*a")
+  -- handle:close()
+
+  if result and result ~= '' then
+    return result:gsub('%s+', '') -- Trim any whitespace
   end
 
   -- Fallback to virtual environments
@@ -27,32 +28,35 @@ pythonPath = function()
   else
     return '/usr/bin/python'
   end
-end;
+end
 
+local python_executable = pythonPath()
 
-require'lspconfig'.pylsp.setup{
+require('lspconfig').pylsp.setup {
   settings = {
     pylsp = {
       plugins = {
         pycodestyle = {
-          ignore = {'W391'},
-          maxLineLength = 100
-        }
-      }
-    }
-  }
+          ignore = { 'W391' },
+          maxLineLength = 100,
+        },
+      },
+    },
+  },
 }
-
 
 vim.lsp.start {
   name = 'pylsp',
   root_dir = vim.fs.dirname(vim.fs.find(root_files, { upward = true })[1]),
-  cmd = { 'pylsp' },
+  cmd = {
+    'pylsp',
+    -- Set the PYTHONPATH environment variable for the LSP
+    env = { PYTHONPATH = python_executable },
+  },
   root_markers = { '.git' },
   filetypes = { 'python' },
   capabilities = require('user.lsp').make_client_capabilities(),
 }
-
 
 -- local dap = require('dap')
 -- dap.adapters.python = function(cb, config)
@@ -81,16 +85,16 @@ vim.lsp.start {
 --     })
 --   end
 -- end
--- 
+--
 -- dap.configurations.python = {
 --   {
 --     -- The first three options are required by nvim-dap
 --     type = 'python'; -- the type here established the link to the adapter definition: `dap.adapters.python`
 --     request = 'launch';
 --     name = "Launch file";
--- 
+--
 --     -- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
--- 
+--
 --     program = "${file}"; -- This configuration will launch the current file if used.
 --     pythonPath = function()
 --       -- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
