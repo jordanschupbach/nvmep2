660c3d1665b6c4e88c2383478402697ea53238ac
jordanschupbach <jordans1882@gmail.com>
2025-10-18 13:33:20 -0600

    Defer R lsp

--- nvim/ftplugin/r.lua
+++ nvim/ftplugin/r.lua
@@ -1,8 +1,15 @@
-vim.lsp.start {
-  name = 'r',
-  root_dir = vim.fs.dirname(vim.fs.find({ '.git' }, { upward = true })[1]),
-  cmd = { 'R', '--slave', '-e', "options(lintr = list(trailing_blank_lines_linter = NULL, snake_case_linter = NULL)); languageserver::run()" },
-  root_markers = { '.clangd', 'compile_commands.json' },
-  filetypes = { 'r', 'R', },
-  capabilities = require('user.lsp').make_client_capabilities(),
-}
+vim.defer_fn(function()
+  vim.lsp.start {
+    name = 'r',
+    root_dir = vim.fs.dirname(vim.fs.find({ '.git' }, { upward = true })[1]),
+    cmd = {
+      'R',
+      '--slave',
+      '-e',
+      'options(lintr = list(trailing_blank_lines_linter = NULL, snake_case_linter = NULL)); languageserver::run()',
+    },
+    root_markers = { '.clangd', 'compile_commands.json' },
+    filetypes = { 'r', 'R' },
+    capabilities = require('user.lsp').make_client_capabilities(),
+  }
+end, 1000)
