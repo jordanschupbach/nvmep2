d9bbd2ea9afe4bffa38d03d745c4bef3abe50bb1
jordanschupbach <jordans1882@gmail.com>
2025-10-18 13:22:30 -0600

    Add print debugging for pylsp

--- nvim/ftplugin/python.lua
+++ nvim/ftplugin/python.lua
@@ -34,6 +34,7 @@ local root_files = {
 -- vim.print('Using Python executable: ' .. python_executable)
 
 vim.defer_fn(function()
+  print('Setting up pylsp LSP server after 2-second delay!')
   require('user.lsp').setup_server('pylsp')
   require('lspconfig').pylsp.setup {
     settings = {
