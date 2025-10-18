57fb5dc61bef3a832bfe526c762ac0709a4675cf
jordanschupbach <jordans1882@gmail.com>
2025-10-18 12:40:16 -0600

    Fix sha for direnv

--- nix/neovim-overlay.nix
+++ nix/neovim-overlay.nix
@@ -1,3 +1,4 @@
+
 # This overlay, when applied to nixpkgs, adds the final neovim derivation to nixpkgs.
 {inputs}: final: prev:
 with final.pkgs.lib; let
@@ -64,7 +65,7 @@ with final.pkgs.lib; let
         owner = "NotAShelf";
         repo = "direnv.nvim";
         rev = "4dfc8758a1deab45e37b7f3661e0fd3759d85788";
-        hash = "sha256-bL33/S+caNmEYGcMLNCanFZyEYUOUmSsedCVBn4tV3g=";
+        hash = "sha256-KqO8uDbVy4sVVZ6mHikuO+SWCzWr97ZuFRC8npOPJIE=";
       };
     };
 
