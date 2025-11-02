b0940d514272b053e63e3de8ddabbd75137d36f0
jordanschupbach <jordans1882@gmail.com>
2025-11-01 22:21:57 -0600

    Add hls config

---
+++ nvim/ftplugin/haskell.lua
@@ -0,0 +1,15 @@
+vim.lsp.start {
+  name = 'hls',
+  cmd = { 'haskell-language-server-wrapper', '--lsp' },
+  filetypes = { 'haskell', 'lhaskell' },
+  root_dir = function(bufnr, on_dir)
+    local fname = vim.api.nvim_buf_get_name(bufnr)
+    on_dir(util.root_pattern('hie.yaml', 'stack.yaml', 'cabal.project', '*.cabal', 'package.yaml')(fname))
+  end,
+  settings = {
+    haskell = {
+      formattingProvider = 'ormolu',
+      cabalFormattingProvider = 'cabalfmt',
+    },
+  },
+}
