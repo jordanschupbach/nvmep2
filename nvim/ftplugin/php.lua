5782a0705d663bd097d62c5b78ed4ae1186e9d6d
jordanschupbach <jordans1882@gmail.com>
2025-11-01 20:50:20 -0600

    Add config

--- nvim/ftplugin/php.lua
+++ nvim/ftplugin/php.lua
@@ -2,15 +2,13 @@
 --   '.git',
 -- }
 
--- vim.lsp.config('phpactor', {
---   cmd = { 'phpactor', 'language-server' },
---   filetypes = { 'php' },
---   root_markers = { '.git' },
---   root_dir = vim.fs.dirname(vim.fs.find(root_files, { upward = true })[1]),
---   -- settings = {}, -- Add any specific settings if needed
---   workspace_required = true,
--- })
-
 vim.lsp.enable {
   'phpactor',
 }
+
+vim.lsp.config('phpactor', {
+  cmd = { 'phpactor', 'language-server' },
+  filetypes = { 'php' },
+  root_markers = { '.git' },
+  workspace_required = true,
+})
