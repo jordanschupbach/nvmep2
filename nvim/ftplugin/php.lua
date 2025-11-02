7396f58dee08a8bbbd4046c776bf7a3f50a12423
jordanschupbach <jordans1882@gmail.com>
2025-11-01 20:16:58 -0600

    Try loading php actor immediately

--- nvim/ftplugin/php.lua
+++ nvim/ftplugin/php.lua
@@ -1,9 +1,7 @@
 
-vim.defer_fn(function()
-  vim.lsp.start {
-    cmd = { 'phpactor', 'language-server' },
-    filetypes = { 'php' },
-    root_markers = { '.git', 'composer.json', '.phpactor.json', '.phpactor.yml' },
-    workspace_required = true,
-  }
-end, 1000)
+vim.lsp.start {
+  cmd = { 'phpactor', 'language-server' },
+  filetypes = { 'php' },
+  root_markers = { '.git', 'composer.json', '.phpactor.json', '.phpactor.yml' },
+  workspace_required = true,
+}
