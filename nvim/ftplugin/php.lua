db70c0522d59b470fae3fa6e4fbce232bd139574
jordanschupbach <jordans1882@gmail.com>
2025-11-01 20:14:16 -0600

    Fix php config

--- nvim/ftplugin/php.lua
+++ nvim/ftplugin/php.lua
@@ -1,11 +1,9 @@
 
 vim.defer_fn(function()
   vim.lsp.start {
-    {
-      cmd = { 'phpactor', 'language-server' },
-      filetypes = { 'php' },
-      root_markers = { '.git', 'composer.json', '.phpactor.json', '.phpactor.yml' },
-      workspace_required = true,
-    }
+    cmd = { 'phpactor', 'language-server' },
+    filetypes = { 'php' },
+    root_markers = { '.git', 'composer.json', '.phpactor.json', '.phpactor.yml' },
+    workspace_required = true,
   }
 end, 1000)
