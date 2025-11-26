bd9de06bac64738df4b8bcd4e62fcc88beca626e
jordanschupbach <jordans1882@gmail.com>
2025-11-25 22:50:14 -0700

    Add refactor treesitter config

--- nvim/plugin/treesitter.lua
+++ nvim/plugin/treesitter.lua
@@ -20,6 +20,27 @@ configs.setup {
       end
     end,
   },
+
+  refactor = {
+    highlight_definitions = {
+      enable = true,
+      -- Set to false if you have an `updatetime` of ~100.
+      clear_on_cursor_move = true,
+    },
+    highlight_current_scope = { enable = true },
+    navigation = {
+      enable = true,
+      -- Assign keymaps to false to disable them, e.g. `goto_definition = false`.
+      keymaps = {
+        goto_definition = 'gnd',
+        list_definitions = 'gnD',
+        list_definitions_toc = 'gO',
+        goto_next_usage = '<a-*>',
+        goto_previous_usage = '<a-#>',
+      },
+    },
+  },
+
   textobjects = {
     select = {
       enable = true,
