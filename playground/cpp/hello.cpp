655027d40dad97c296832111bb2f92ee02fe5d46
jordanschupbach <jordans1882@gmail.com>
2025-11-30 11:36:02 -0700

    Add dap for cpp

--- playground/cpp/hello.cpp
+++ playground/cpp/hello.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 
+void say_helloer() { std::cout << "Hello world" << std::endl; }
+
 int main(void) {
+  say_helloer();
   std::cout << "Hello world" << std::endl;
 }
--- playground/python/hello.py
+++ playground/python/hello.py
@@ -4,5 +4,8 @@ import numpy as np
 print("hello, world!!!")
 
 x = np.random.rand(100)
-
 x
+
+y = np.sin(x)
+y
+
