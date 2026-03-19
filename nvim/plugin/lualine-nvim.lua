de6741e7d46c2123e3d4d13b3c36f93bb928f0ae
jordan <jordans1882@gmail.com>
2026-03-19 08:34:19 -0600

    Modify line

--- nvim/plugin/lualine-nvim.lua
+++ nvim/plugin/lualine-nvim.lua
@@ -33,11 +33,11 @@ require('lualine').setup {
   },
   sections = {
     lualine_a = { 'mode' },
-    lualine_b = { 'branch', 'diff', 'diagnostics' },
-    lualine_c = { 'filename' },
-    lualine_x = { 'encoding', 'fileformat', 'filetype' },
-    lualine_y = { 'progress' },
-    lualine_z = { 'location' },
+    lualine_b = {},
+    lualine_c = {},
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = {},
   },
   inactive_sections = {
     lualine_a = {},
