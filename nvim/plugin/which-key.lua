bf64023b62af27e4009c72e072efd95b1736aadb
jordan <jordans1882@gmail.com>
2026-03-18 22:29:19 -0600

    Add more icons

--- nvim/plugin/which-key.lua
+++ nvim/plugin/which-key.lua
@@ -3,7 +3,7 @@ require('which-key').setup {
   -- preset = 'helix'
 }
 local wk = require('which-key')
--- 󱪳󰟝󱕘
+-- 󱪳󰟝󱕘 󱡶󱜚󱜙󱘎󱓞󱓟󱒉󱎯󱍊󱇪󱇫󱁾󱁕󱁊󱁉󰻈󰺛󰸘󰸗󰴻󰴺󰴭󰳆󰱐󰭎󰨡󰨆󰨊󰨈󰧲󰧱󰧰󰧊󰧑󰥨󰥩󰤑󰣙󰣘󰡴󰟁󰜏󰙨󰑣󰒪󰐅󰐁󰌢󰋸󰊢󰅎󰅏󰃤󰂒󰂓󰂖󰂚󰂞⭘
 wk.add {
   { '<Space>a', group = 'Aerial', icon = '' },
   { '<Space>b', group = 'Buffers' },
@@ -11,14 +11,15 @@ wk.add {
   { '<Space>d', group = 'Debug', icon = '' },
   { '<Space>e', group = 'Errors', icon = '' },
   { '<Space>e', group = 'File', icon = '' },
-  { '<Space>g', group = 'Git/go' },
+  { '<Space>g', group = 'Git/go', icon = '' },
   { '<Space>h', group = 'Help', icon = '' },
   { '<Space>m', group = 'Make' },
   { '<Space>o', group = 'Open' },
-  { '<Space>p', group = 'Project' },
+  { '<Space>p', group = 'Project', icon = '' },
   { '<Space>r', group = 'Run', icon = '' },
   { '<Space>t', group = 'Tab' },
   { '<Space>u', group = 'Ui' },
   { '<Space>w', group = 'Window/Workspace', icon = '󱪳' },
   { '<Space>y', group = 'Snippet' },
+  { '<Space>z', icon = '', command = '<CMD>ZenMode<CR>' },
 }
