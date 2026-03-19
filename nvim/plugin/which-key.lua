4d5b92cf00ed5fe5a952899e39ddd0bf147f7348
jordan <jordans1882@gmail.com>
2026-03-19 07:57:42 -0600

    Add more icons

--- nvim/plugin/which-key.lua
+++ nvim/plugin/which-key.lua
@@ -8,12 +8,11 @@ local wk = require('which-key')
 -- 
 -- ⭘
 -- 
--- 󰂒󰃇󰃤󰃠󰃠󰃝󰄰󰄯󰈝󰈪 
+-- 󰂒󰃇󰃤󰃠󰃠󰃝󰄰󰄯󰈝󰈪  
 
 wk.add {
   { '<Space>a', group = 'Aerial', icon = '' },
-  { '<Space>b', group = 'Buffers' },
-  icon = '',
+  { '<Space>b', group = 'Buffers', icon = '' },
   { '<Space>c', group = 'Code', icon = '' },
   { '<Space>d', group = 'Debug', icon = '' },
   { '<Space>e', group = 'Errors', icon = '' },
@@ -24,9 +23,12 @@ wk.add {
   { '<Space>o', group = 'Open', icon = '' },
   { '<Space>p', group = 'Project', icon = '' },
   { '<Space>r', group = 'Run', icon = '' },
-  { '<Space>t', group = 'Tab' },
-  { '<Space>u', group = 'Ui', icon = '󰧊' },
-  { '<Space>w', group = 'Window/Workspace', icon = '󱪳' },
-  { '<Space>y', group = 'Snippet' },
+  { '<Space>t', group = 'Tab', icon = '' },
+  { '<Space>u', group = 'Ui', icon = '' },
+  { '<Space>w', group = 'Window/Workspace', icon = '' },
+  { '<Space>y', group = 'Snippet', icon = '' },
   { '<Space>z', '<CMD>ZenMode<CR>', mode = 'n', icon = '' },
+  { '<Space><return>', '<CMD>Nuake<CR>', mode = 'n', icon = '' },
+  { '<Space><Space>', '<CMD>JustRun<CR>', mode = 'n', icon = '󱕘' },
+  { '<Space>j', '<CMD>JustSelect<CR>', mode = 'n', icon = '' },
 }
