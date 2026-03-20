c326974f2cd357c98d902d53c3d9f40b60ad0007
jordan <jordans1882@gmail.com>
2026-03-19 20:47:51 -0600

    Add dap bindings in js

--- nvim/ftplugin/typescript.lua
+++ nvim/ftplugin/typescript.lua
@@ -32,3 +32,14 @@ for _, language in ipairs { 'typescript', 'javascript' } do
     },
   }
 end
+
+mymap('n', '<Space>du', '<CMD>lua require"dapui".toggle()<CR>')
+mymap('n', '<Space>db', '<CMD>DapToggleBreakpoint<CR>')
+mymap('n', '<Space>dd', '<CMD>DapContinue<CR>')
+mymap('n', '<Space>dc', '<CMD>DapContinue<CR>')
+mymap('n', '<Space>dO', '<CMD>DapStepOut<CR>')
+mymap('n', '<Space>di', '<CMD>DapStepInto<CR>')
+mymap('n', '<Space>do', '<CMD>DapStepOver<CR>')
+mymap('n', '<Space>dC', '<CMD>DapClearBreakpoints<CR>')
+mymap('n', '<Space>dR', '<CMD>DapRestartFrame<CR>')
+mymap('n', '<Space>dP', '<CMD>DapPause<CR>')
