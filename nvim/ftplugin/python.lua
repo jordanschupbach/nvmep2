7a33d82c77c54739fa429905844f6e3904fe98c5
jordan <jordans1882@gmail.com>
2026-03-19 20:21:29 -0600

    Attempt to add python dap

--- nvim/ftplugin/python.lua
+++ nvim/ftplugin/python.lua
@@ -61,34 +61,34 @@ vim.defer_fn(function()
   }
 end, 1000)
 
--- local dap = require('dap')
--- dap.adapters.python = function(cb, config)
---   if config.request == 'attach' then
---     ---@diagnostic disable-next-line: undefined-field
---     local port = (config.connect or config).port
---     ---@diagnostic disable-next-line: undefined-field
---     local host = (config.connect or config).host or '127.0.0.1'
---     cb({
---       type = 'server',
---       port = assert(port, '`connect.port` is required for a python `attach` configuration'),
---       host = host,
---       options = {
---         source_filetype = 'python',
---       },
---     })
---   else
---     cb({
---       type = 'executable',
---       -- command = 'path/to/virtualenvs/debugpy/bin/python',
---       command = pythonPath(),
---       args = { '-m', 'debugpy.adapter' },
---       options = {
---         source_filetype = 'python',
---       },
---     })
---   end
--- end
---
+local dap = require('dap')
+dap.adapters.python = function(cb, config)
+  if config.request == 'attach' then
+    ---@diagnostic disable-next-line: undefined-field
+    local port = (config.connect or config).port
+    ---@diagnostic disable-next-line: undefined-field
+    local host = (config.connect or config).host or '127.0.0.1'
+    cb {
+      type = 'server',
+      port = assert(port, '`connect.port` is required for a python `attach` configuration'),
+      host = host,
+      options = {
+        source_filetype = 'python',
+      },
+    }
+  else
+    cb {
+      type = 'executable',
+      -- command = 'path/to/virtualenvs/debugpy/bin/python',
+      command = pythonPath(),
+      args = { '-m', 'debugpy.adapter' },
+      options = {
+        source_filetype = 'python',
+      },
+    }
+  end
+end
+
 -- dap.configurations.python = {
 --   {
 --     -- The first three options are required by nvim-dap
