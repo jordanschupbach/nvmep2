4ecca721d296e9c0a0e4fae24d2b8021112e7697
jordan <jordans1882@gmail.com>
2026-03-19 20:59:02 -0600

    Remove extra curly braces

--- nvim/ftplugin/javascript.lua
+++ nvim/ftplugin/javascript.lua
@@ -19,20 +19,11 @@ require('dap-vscode-js').setup {
 for _, language in ipairs { 'typescript', 'javascript' } do
   require('dap').configurations[language] = {
     {
-      -- {
-      --   type = 'pwa-node',
-      --   request = 'launch',
-      --   name = 'Launch file',
-      --   program = '${file}',
-      --   cwd = '${workspaceFolder}',
-      -- },
-      {
-        type = 'pwa-node',
-        request = 'attach',
-        name = 'Attach',
-        processId = require('dap.utils').pick_process,
-        cwd = '${workspaceFolder}',
-      },
+      type = 'pwa-node',
+      request = 'attach',
+      name = 'Attach',
+      processId = require('dap.utils').pick_process,
+      cwd = '${workspaceFolder}',
     },
   }
 end
