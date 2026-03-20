81c59fd684a4a846f98921529b4622bb7a601631
jordan <jordans1882@gmail.com>
2026-03-19 19:58:28 -0600

    Attempt to add none-ls config

--- nvim/plugin/none-ls.lua
+++ nvim/plugin/none-ls.lua
@@ -1,4 +1,3 @@
-
 local null_ls = require('null-ls')
 null_ls.setup {
   sources = {
@@ -76,14 +75,14 @@ null_ls.setup {
 
     -- {{{ python
 
-    -- null_ls.builtins.diagnostics.pylint.with {
-    --   diagnostics_postprocess = function(diagnostic)
-    --     diagnostic.code = diagnostic.message_id
-    --   end,
-    -- },
+    null_ls.builtins.diagnostics.pylint.with {
+      diagnostics_postprocess = function(diagnostic)
+        diagnostic.code = diagnostic.message_id
+      end,
+    },
     -- null_ls.builtins.formatting.isort,
-    -- null_ls.builtins.formatting.black,
-    -- null_ls.builtins.diagnostics.mypy,
+    null_ls.builtins.formatting.black,
+    null_ls.builtins.diagnostics.mypy,
 
     -- }}} python
   },
