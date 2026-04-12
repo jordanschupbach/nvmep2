71a192a177e1647c56726908732de57c5339d2c8
jordanschupbach <jordans1882@gmail.com>
2026-04-12 08:23:18 -0600

    hard code more org roam dirs

--- nvim/plugin/ot_org-roam.lua
+++ nvim/plugin/ot_org-roam.lua
@@ -2,7 +2,7 @@ require('org-roam').setup {
   directory = '/home/jordan/org-roam',
   -- optional
   org_files = {
-    -- '~/org-roam',
+    '/home/jordan/org-roam',
     -- "~/another_org_dir",
     -- "~/some/folder/*.org",
     -- "~/a/single/org_file.org",
