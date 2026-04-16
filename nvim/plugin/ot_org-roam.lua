38cfca3a64bff4943ca6a605bb298d5b63a7b83f
jordanschupbach <jordans1882@gmail.com>
2026-04-16 07:58:22 -0600

    Add org roam bindings

--- nvim/plugin/ot_org-roam.lua
+++ nvim/plugin/ot_org-roam.lua
@@ -7,4 +7,14 @@ require('org-roam').setup {
     -- "~/some/folder/*.org",
     -- "~/a/single/org_file.org",
   },
+  bindings = {
+    find_node = '<Space>orr',
+    insert_node = '<Space>ori',
+    toggle_roam_buffer_fixed = '<Space>orb',
+    capture_date = '<Space>orcD',
+    goto_date = '<Space>orgD',
+    capture = '<Space>orcc',
+    goto_next_node = '<Space>orn',
+    goto_prev_node = '<Space>orp',
+  },
 }
