9368790bf282743d85d8df47f176cf010092f553
jordan <jordans1882@gmail.com>
2026-04-19 09:35:12 -0600

    Attempt to use just jdtls lsp

--- nvim/ftplugin/java.lua
+++ nvim/ftplugin/java.lua
@@ -118,9 +118,15 @@ local config = {
     bundles = {},
   },
 }
+
+vim.lsp.start {
+  name = 'jdtls',
+  cmd = { 'jdtls' },
+}
+
 -- This starts a new client & server,
 -- or attaches to an existing client & server depending on the `root_dir`.
-require('jdtls').start_or_attach(config)
+-- require('jdtls').start_or_attach(config)
 
 -- {{{ old config
 
